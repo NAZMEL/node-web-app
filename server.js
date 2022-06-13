28213511d9ffbafc48da0dc5a44ff003c244895d
NAZMEL <melnyknasar@gmail.com>
2022-06-13 14:36:28 +0300

    Add middlewares

--- server.js
+++ server.js
@@ -2,19 +2,50 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.set("view engine", "ejs");
+
 const PORT = 3000;
-const createPath = (page) => path.resolve(__dirname, "views", `${page}.html`);
+const createPath = (page) =>
+  path.resolve(__dirname, "ejs-views", `${page}.ejs`);
 
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log("listenint port 3000");
 });
 
+app.use((req, res, next) =>{
+    console.log(`path: ${req.path}`);
+    console.log(`method: ${req.method}`);
+    next();    
+})
+
 app.get("/", (req, res) => {
-  res.sendFile(createPath("index"));
+  const title = "Home";
+  res.render(createPath("index"), { title });
 });
 
 app.get("/contacts", (req, res) => {
-  res.sendFile(createPath("contacts"));
+  const title = "Contacts";
+  const contacts = [
+    { name: "YouTube", link: "http://youtube.com/YauhenKavalchuk" },
+    { name: "Twitter", link: "http://github.com/YauhenKavalchuk" },
+    { name: "GitHub", link: "http://twitter.com/YauhenKavalchuk" },
+  ];
+  res.render(createPath("contacts"), { contacts, title });
+});
+
+app.get("/posts/:id", (req, res) => {
+  const title = "Post";
+  res.render(createPath("posts"), { title });
+});
+
+app.get("/posts", (req, res) => {
+  const title = "Posts";
+  res.render(createPath("posts"), { title });
+});
+
+app.get("/add-post", (req, res) => {
+  const title = "Add post";
+  res.render(createPath("add-post"), { title });
 });
 
 app.get("/about-us", (req, res) => {
@@ -22,5 +53,6 @@ app.get("/about-us", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(createPath("error"));
+  const title = "Error";
+  res.status(404).render(createPath("error"), { title });
 });
