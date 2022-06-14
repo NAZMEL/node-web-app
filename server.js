dc2a3399823b4ffc6b5a4ba73c951869eff91d4f
NAZMEL <melnyknasar@gmail.com>
2022-06-14 13:42:35 +0300

    Connect contacts and new posts to DB

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Post = require("./models/post");
+const Contact = require("./models/contact");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -37,52 +38,45 @@ app.get("/", (req, res) => {
 
 app.get("/contacts", (req, res) => {
   const title = "Contacts";
-  const contacts = [
-    { name: "YouTube", link: "http://youtube.com/YauhenKavalchuk" },
-    { name: "Twitter", link: "http://github.com/YauhenKavalchuk" },
-    { name: "GitHub", link: "http://twitter.com/YauhenKavalchuk" },
-  ];
-  res.render(createPath("contacts"), { contacts, title });
+  Contact.find()
+    .then((contacts) => res.render(createPath('contacts'), { contacts, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' })
+    });
 });
 
 app.get("/posts/:id", (req, res) => {
   const title = "Post";
-  const post = {
-    id: '1',
-    title: 'Lorem',
-    text: 'Why do we use it?It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).Where does it come from?Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham',
-    date: '14.06.2022',
-    author: 'Bill',
-  }
-  res.render(createPath("post"), { title, post });
+  Post.findById(req.params.id)
+    .then((post) => res.render(createPath('post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' })
+    });
 });
 
 app.get("/posts", (req, res) => {
   const title = "Posts";
-  const posts = [
-    {
-      id: '1',
-      title: 'Lorem',
-      text: 'Why do we use it?It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).Where does it come from?Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham',
-      date: '14.06.2022',
-      author: 'Bill',
-    },
-  ]
-  res.render(createPath("posts"), { title, posts });
+  Post.find()
+    .sort({ createdAt: -1 })
+    .then((posts) => res.render(createPath('posts'), { posts, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' })
+    });
 });
 
 app.post("/add-post", (req, res) => {
   const { title, author, text } = req.body;
   const post = new Post({ title, author, text });
-  
+
   post.save()
-    .then((data) => res.send(data))
+    .then(() => res.redirect('/posts'))
     .catch((error) => {
       console.log(error);
       res.render(createPath('error'), { title: 'Error' })
     });
-
-  res.render(createPath('post'), { post, title });
 });
 
 app.get("/add-post", (req, res) => {
